4372731b3af4c2e04b5dcc6a1d94f7553ebd8a2d
oğulcan <karayel.ogulcan1903@gmail.com>
2020-09-15 17:03:03 +0300

    some editing in app.tsx

--- App.tsx
+++ App.tsx
@@ -2,8 +2,13 @@
 
 import React from "react";
 import LoadAssets from "./src/components";
+import { createStackNavigator } from "@react-navigation/stack";
 import { ThemeProvider, Box,Text } from "./src/utils";
 import { useOrientation } from './src/hooks/useOrientation';
+import { AppRoutes } from "./src/types/navigation-type";
+import { AuthenticationNavigator } from "./src/navigation/auth-navigation";
+import { useFonts } from "expo-font";
+import { AppLoading } from "expo";
 
 
 
@@ -12,7 +17,11 @@ import { useOrientation } from './src/hooks/useOrientation';
 
 
 const fonts = {
-  RobotoRegular: require("./assets/fonts/Roboto-Regular.ttf"),
+  "RobotoRegular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Anton":require("./assets/fonts/Anton-Regular.ttf"),
+    "CrimsonBold":require("./assets/fonts/CrimsonText-Bold.ttf"),
+    "CrimsonRegular":require("./assets/fonts/CrimsonText-Regular.ttf"),
+    "Oswald":require("./assets/fonts/Oswald-Bold.ttf")
 };
 
 const assets = [
@@ -20,13 +29,15 @@ const assets = [
   "https://res.cloudinary.com/ogulcankarayel-digital/image/upload/v1599030254/2892856-removebg_1_1_vl6gyl.png",
 ];
 
+const AppStack = createStackNavigator<AppRoutes>()
 export default function App() {
-  
-  const orientation = useOrientation();
+ 
   return (
     <ThemeProvider>
-      <LoadAssets {...{ fonts, assets }}>
-      
+      <LoadAssets {...{ assets,fonts }}>
+        <AppStack.Navigator headerMode="none">
+          <AppStack.Screen name="Authentication" component={AuthenticationNavigator}/>
+        </AppStack.Navigator>
       </LoadAssets>
     </ThemeProvider>
   );
