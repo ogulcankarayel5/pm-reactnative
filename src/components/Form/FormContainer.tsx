9202f42f222881ec84b14f0f42f62a06b02060fb
oğulcan <karayel.ogulcan1903@gmail.com>
2020-10-01 16:21:57 +0300

    formcontainer added

---
+++ src/components/Form/FormContainer.tsx
@@ -0,0 +1,39 @@
+import React, { ReactNode } from "react";
+import { Image } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { Box, Text } from "../../utils";
+import SafeAreaView from "../SafeAreaView";
+
+type FormContainerProps = {
+  title: string;
+  children: ReactNode;
+  footer: ReactNode;
+};
+
+const FormContainer = ({ title, children, footer }: FormContainerProps) => {
+  
+  return (
+    <SafeAreaView>
+      <KeyboardAwareScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <Box flex={1} backgroundColor="white">
+          <Box flex={0.2} alignItems="center" justifyContent="center">
+            <Image
+              source={require("../../../assets/splash.png")}
+              style={{ width: 300, height: 80 }}
+            />
+          </Box>
+
+          <Box flex={1} paddingHorizontal="l">
+            <Box flex={1} justifyContent="center">
+              <Text variant="formTitle">{title}</Text>
+              {children}
+            </Box>
+            {footer}
+          </Box>
+        </Box>
+      </KeyboardAwareScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default FormContainer;
