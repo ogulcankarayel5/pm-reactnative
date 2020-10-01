dbccc9931facaad8dbd3e64880d35b5337d1c11c
oğulcan <karayel.ogulcan1903@gmail.com>
2020-10-01 16:23:13 +0300

    form footer added

---
+++ src/components/Form/Footer.tsx
@@ -0,0 +1,46 @@
+import React, { ReactNode } from "react";
+import { BorderlessButton } from "react-native-gesture-handler";
+import { BaseProps, BaseTextProps } from "../../../types";
+import { Box, Text } from "../../utils";
+
+type FooterProps = BaseProps & {
+  children: ReactNode;
+};
+
+export const Footer = ({ children, ...props }: FooterProps) => {
+  return (
+    <Box flex={1} justifyContent="space-around" mt="m" {...props}>
+      {children}
+    </Box>
+  );
+};
+
+type FooterTextProps = BaseTextProps & {
+  children: string;
+};
+export const FooterText = ({ children, ...props }: FooterTextProps) => {
+  return (
+    <Text mb="m" variant="authText" {...props}>
+      {`-${children}-`}{" "}
+    </Text>
+  );
+};
+
+type FooterActionProps = {
+  onPress: () => void;
+  title: string;
+  action: string;
+};
+
+export const FooterAction = ({ onPress, title, action }: FooterActionProps) => {
+  return (
+    <BorderlessButton {...{ onPress }}>
+      <Text variant="authText">
+        {title}
+        <Text ml="xl" color="authAction">
+          {action}
+        </Text>
+      </Text>
+    </BorderlessButton>
+  );
+};
