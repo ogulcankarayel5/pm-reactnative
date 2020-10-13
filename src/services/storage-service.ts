00cbd55e29980a54eadfcc565efe68df9ec3fe21
oğulcan <karayel.ogulcan1903@gmail.com>
2020-10-13 22:57:48 +0300

    storage service added

---
+++ src/services/storage-service.ts
@@ -0,0 +1,27 @@
+import AsyncStorage from "@react-native-community/async-storage";
+
+const setToken = async (
+  accessToken: string,
+  refreshToken: string
+): Promise<void> => {
+  try {
+    await AsyncStorage.setItem("@lucky_access_token", accessToken);
+    await AsyncStorage.setItem("@lucky_refresh_token", refreshToken);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getToken = async (tokenName: string): Promise<string> => {
+  try {
+    const token = await AsyncStorage.getItem(tokenName);
+    if (token != null) {
+      return token;
+    }
+    return null;
+  } catch (error) {
+    return error;
+  }
+};
+
+export default { setToken, getToken };
