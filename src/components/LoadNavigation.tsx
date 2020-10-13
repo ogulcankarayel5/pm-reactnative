9dbeccead8effa890476c2b259bba3183226788a
oğulcan <karayel.ogulcan1903@gmail.com>
2020-10-13 23:02:18 +0300

    showing loading if loading is true

--- src/components/LoadNavigation.tsx
+++ src/components/LoadNavigation.tsx
@@ -3,20 +3,26 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { AppLoading } from "expo";
 import React, { useCallback } from "react";
 import { AuthenticationNavigator } from "../navigation/auth-navigation";
+import { useTypedSelector } from "../redux";
 import { Onboarding } from "../screens";
 import { AppRoutes } from "../types/navigation-type";
 import { useIsFirstLaunch } from "./../hooks/useIsFirstLaunch";
+import Loading from "./Loading";
 
 const AppStack = createStackNavigator<AppRoutes>();
 
 const LoadNavigation = () => {
+  const { loading } = useTypedSelector((state) => ({
+    loading: state.auth.loading,
+  }));
+
   const { isFirst, changeValue } = useIsFirstLaunch();
   // const { isFirst } = useIsFirstLaunch();
   console.log(isFirst);
 
   const onDone = useCallback(async () => {
     try {
-      await AsyncStorage.setItem("first27", JSON.stringify(true));
+      await AsyncStorage.setItem("first29", JSON.stringify(true));
       changeValue(false);
     } catch (err) {
       console.log(err);
@@ -29,6 +35,8 @@ const LoadNavigation = () => {
 
   return isFirst ? (
     <Onboarding onDone={onDone} />
+  ) : loading ? (
+    <Loading />
   ) : (
     <AppStack.Navigator headerMode="none">
       <AppStack.Screen
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,9 @@
 export { default as Button } from "./Button";
 export { default as Dot } from "./Dot";
 export * from "./Form";
+export * from "./icons";
 export { default as LoadAssets } from "./LoadAssets";
+export { default as Loading } from "./Loading";
 export { default as LoadNavigation } from './LoadNavigation';
 export { default as SafeAreaView } from "./SafeAreaView";
 export { SocialIcon, SocialIconButton, SocialIcons } from "./SocialAuth";
