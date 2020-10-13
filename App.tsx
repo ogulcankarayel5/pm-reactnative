e454152896d650aa4d6f98fdba44dcbfa4d6ba60
oğulcan <karayel.ogulcan1903@gmail.com>
2020-10-13 22:55:56 +0300

    LoadApp added

--- App.tsx
+++ App.tsx
@@ -1,28 +1,18 @@
 import React from "react";
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { LoadAssets, LoadNavigation } from "./src/components";
-import { fonts, onboarding1, onboarding2 } from "./src/constants";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Provider } from "react-redux";
+import { LoadApp } from "./LoadApp";
+import { store } from "./src/redux/store/index";
 import { ThemeProvider } from "./src/utils";
 
-
-//LoadAssets font,navigation ve asset caching için
-//assets = [require(./...)] and use it assets[0]
-const assets = [
-  require("./assets/splash.png"),
-  onboarding1,
-  onboarding2,
-];
-
-
 export default function App() {
- 
   return (
-  <SafeAreaProvider>
-      <ThemeProvider>
-      <LoadAssets {...{ assets, fonts }}>
-       <LoadNavigation/>
-     </LoadAssets>
-    </ThemeProvider>
-  </SafeAreaProvider>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <ThemeProvider>
+          <LoadApp />
+        </ThemeProvider>
+      </SafeAreaProvider>
+    </Provider>
   );
 }
