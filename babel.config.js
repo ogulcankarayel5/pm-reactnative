ede05cd0fb4b23fa8c97b23a59c4933c886e132e
oğulcan <karayel.ogulcan1903@gmail.com>
2020-12-02 23:11:44 +0300

    module-resolver and tslint added

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,48 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
-      ["module:react-native-dotenv", {
-        "moduleName": "@env",
-        "path": ".env",
-        "blacklist": null,
-        "whitelist": null,
-        "safe": false,
-        "allowUndefined": true
-      }]
-    ]
-    
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+      [
+        "module-resolver",
+        {
+          root: ["."],
+          extensions: [
+            ".ios.ts",
+            ".android.ts",
+            ".ts",
+            ".ios.tsx",
+            ".android.tsx",
+            ".tsx",
+            ".jsx",
+            ".js",
+            ".json",
+          ],
+          alias: {
+            "assets": "./assets",
+            "components": "./src/components",
+            "constants": "./src/constants",
+            "data": "./src/data",
+            "hooks": "./src/hooks",
+            "store": "./src/store",
+            "screens": "./src/screens",
+            "services": "./src/services",
+            "types": "./src/types",
+            "utils": "./src/utils",
+          },
+        },
+      ],
+    ],
   };
 };
